d54b87ac44fbe6aa988384a5aebc8c4a0d17f0dc
Rene Morales <renemorales.developer@gmail.com>
2021-06-21 10:55:35 -0500

    project setup

---
+++ app.js
@@ -0,0 +1,9 @@
+// Define Ui Vars
+
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// load all event lisiteners
