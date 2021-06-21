90f396ed0ce99bc01c67a64cdab514703e9365d0
Rene Morales <renemorales.developer@gmail.com>
2021-06-21 11:29:41 -0500

    created a function to Add task

--- app.js
+++ app.js
@@ -12,4 +12,36 @@ loadEventListeners();
 //  load all event listeners
 function loadEventListeners() {
   // add task event
+  form.addEventListener("submit", addTask);
+}
+
+// Add task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("add a task");
+  }
+
+  //create li element
+  const li = document.createElement("li");
+  // add class to li
+  li.className = "collection-item";
+  // create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // create new link element
+  const link = document.createElement("a");
+  // add class to link
+  link.className = "delete-item secondary-content";
+  // add icon html
+  // link.innerHTML = '<i class="fa fa-remove"></i>';
+  link.innerHTML = '<i class="far fa-trash-alt"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // append the li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = "";
+
+  e.preventDefault();
 }
